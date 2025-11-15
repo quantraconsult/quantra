6aedef58eba0adfc3fa504cc9d67c4aa9b2ff85a
quantraconsult <quantraconsult@gmail.com>
2025-11-15 14:09:55 +0200

    logo rename

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const QuantraLogo: React.FC<{ className?: string, alt?: string }> = ({ className, alt = "Quantra Logo" }) => {
     return (
-        <img public="/Quantra-logo.png" alt={alt} className={className} />
+        <img public="/quantra-logo.png" alt={alt} className={className} />
     );
 };
