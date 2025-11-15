830185d304da5762d29e0b1a2570bb4138e1b972
quantraconsult <quantraconsult@gmail.com>
2025-11-15 14:17:52 +0200

    logo.tsx change

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export const QuantraLogo: React.FC<{ className?: string, alt?: string }> = ({ className, alt = "Quantra Logo" }) => {
-    return (
-        <img public="/quantra-logo.png" alt={alt} className={className} />
-    );
+export const QuantraLogo: React.FC<{ className?: string; alt?: string }> = ({
+  className,
+  alt = 'Quantra Logo',
+}) => {
+  return (
+    <img
+      src="/quantra-logo.png"
+      alt={alt}
+      className={className}
+    />
+  );
 };
+
