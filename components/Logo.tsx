750fddab77b135de7551f54e258afd7b510b1231
quantraconsult <quantraconsult@gmail.com>
2025-11-15 14:05:08 +0200

    Move logo to public and fix path

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const QuantraLogo: React.FC<{ className?: string, alt?: string }> = ({ className, alt = "Quantra Logo" }) => {
     return (
-        <img src="/Quantra-logo.png" alt={alt} className={className} />
+        <img public="/Quantra-logo.png" alt={alt} className={className} />
     );
 };
